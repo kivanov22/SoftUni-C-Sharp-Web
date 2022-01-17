d8be76a1ee9aaf20e90586c79a7493cc464a595d
Kristian Ivanov <kriskopk@abv.bg>
2022-01-17 20:15:45 +0200

    BasicWebServer

---
+++ C-Sharp-Web-Basics/Simple HTTP Server/BasicWebServer.Server/HTTP/HeaderCollection.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicWebServer.Server.HTTP
+{
+    public class HeaderCollection:IEnumerable<Header>
+    {
+        private readonly Dictionary<string, Header> headers;
+
+        public HeaderCollection () =>this.headers= new Dictionary<string, Header> ();
+
+        public string this[string name] 
+            => this.headers[name].Value;
+
+        public int Count => this.headers.Count;
+
+        public bool Contains(string name)
+            => this.headers.ContainsKey(name);
+
+        public void Add(string name,string value)=> this.headers[name]= new Header(name,value); 
+        //{
+        //    var header = new Header(name, value);
+
+        //    this.headers.Add(name, header);
+        //}
+
+        public IEnumerator<Header> GetEnumerator() =>this.headers.Values.GetEnumerator();
+        
+
+        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+        
+    }
+}
---
+++ C-Sharp-Web-Basics/Simple HTTP Server/BasicWebServer.Server/Responses/TextResponse.cs
@@ -0,0 +1,13 @@
+﻿using BasicWebServer.Server.HTTP;
+
+namespace BasicWebServer.Server.Responses
+{
+    public class TextResponse : ContentResponse
+    {
+        public TextResponse(string text,
+            Action<Request, Response> preRenderAction = null) 
+            : base(text, ContentType.PlainText,preRenderAction)
+        {
+        }
+    }
+}
