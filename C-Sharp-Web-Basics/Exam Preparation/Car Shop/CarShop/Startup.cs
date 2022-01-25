83057ccbe8d0d7325c98bbe0cec816f502a2d3fd
Kristian Ivanov <kriskopk@abv.bg>
2022-01-25 18:57:00 +0200

    Exam prep Car Shop

---
+++ C-Sharp-Web-Basics/Exam Preparation/Car Shop/CarShop/Startup.cs
@@ -0,0 +1,21 @@
+﻿
+namespace CarShop
+{
+    using MyWebServer;
+    using MyWebServer.Controllers;
+    using MyWebServer.Results.Views;
+    using System;
+    using System.Threading.Tasks;
+
+    public class Startup
+    {
+        public static async Task Main()
+            => await HttpServer
+            .WithRoutes(routes => routes
+            .MapStaticFiles()
+            .MapControllers())
+            .WithServices(services => services
+            .Add<IViewEngine, CompilationViewEngine>())
+            .Start();
+    }
+}
