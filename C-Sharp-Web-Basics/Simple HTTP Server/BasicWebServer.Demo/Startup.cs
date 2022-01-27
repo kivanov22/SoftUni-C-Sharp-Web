61248d65780a1afb277048c20765c1129dd8212d
Kristian Ivanov <kriskopk@abv.bg>
2022-01-27 15:13:11 +0200

    Controller functionality implemented

--- C-Sharp-Web-Basics/Simple HTTP Server/BasicWebServer.Demo/Startup.cs
+++ C-Sharp-Web-Basics/Simple HTTP Server/BasicWebServer.Demo/Startup.cs
@@ -30,22 +30,26 @@ public class Startup
 
     public static async Task Main()
     {
-        await DownloadSitesAsTextFile(Startup.FileName,
-            new string[] { "https://judge.softuni.org/", "https://softuni.org/" });
-
-        var server = new HttpServer(routes => routes
-         .MapGet("/", new TextResponse("Hello from the server!"))
-         .MapGet("/Redirect", new RedirectResponse("https://softuni.org/"))
-         .MapGet("/HTML", new HtmlResponse(Startup.HtmlForm))
-         .MapPost("/HTML", new TextResponse("", Startup.AddFormDataAction))
-         .MapGet("/Content", new HtmlResponse(Startup.DownloadForm))
-         .MapPost("/Content", new TextFileResponse(Startup.FileName))
-         .MapGet("/Cookies", new HtmlResponse("",Startup.AddCookiesAction))
-         .MapGet("/Session",new TextResponse("",Startup.DisplaySessionInfoAction))
-         .MapGet("/Login",new HtmlResponse(Startup.LoginForm))
-         .MapPost("/Login",new HtmlResponse("",Startup.LoginAction))
-         .MapGet("/Logout", new HtmlResponse("",Startup.LogoutAction))
-         .MapGet("/UserProfile", new HtmlResponse("",Startup.GetUserDataAction)));
+        
+            await new HttpServer(routes => routes
+         .MapGet<HomeController>("/",c=>c.Index())
+         .MapGet<HomeController>("/Redirect",c=>c.Redirect())
+         .MapGet<HomeController>("/HTML",c=>c.Html())
+         .MapPost<HomeController>("/HTML",c=>c.HtmlFormPost())
+         .MapGet<HomeController>("/Content"), c=>c.Content())
+         .MapPost<HomeController>("/Content"), c=>c.DownloadContent())
+         .MapGet<HomeController>("/Cookies"), c=>c.Cookies())
+         .MapGet<HomeController>("/Session"), c=>c.Session())
+              
+         //.MapGet("/HTML", new HtmlResponse(Startup.HtmlForm))
+         //.MapGet("/Content", new HtmlResponse(Startup.DownloadForm))
+         //.MapPost("/Content", new TextFileResponse(Startup.FileName))
+         //.MapGet("/Cookies", new HtmlResponse("",Startup.AddCookiesAction))
+         //.MapGet("/Session",new TextResponse("",Startup.DisplaySessionInfoAction))
+         //.MapGet("/Login",new HtmlResponse(Startup.LoginForm))
+         //.MapPost("/Login",new HtmlResponse("",Startup.LoginAction))
+         //.MapGet("/Logout", new HtmlResponse("",Startup.LogoutAction))
+         //.MapGet("/UserProfile", new HtmlResponse("",Startup.GetUserDataAction)));
 
           await server.Start();
 
---
+++ C-Sharp-Web-Basics/Simple HTTP Server/BasicWebServer.Server/Routing/RoutingTableExtensions.cs
@@ -0,0 +1,28 @@
+﻿namespace BasicWebServer.Server.Routing
+{
+    using BasicWebServer.Server.Controllers;
+    using BasicWebServer.Server.HTTP;
+    public static class RoutingTableExtensions
+    {
+        public static IRoutingTable MapGet<TController>(
+            this IRoutingTable routingTable,
+            string path,
+            Func<TController, Response> controllerFunction)
+            where TController : Controller
+            => routingTable.MapGet(path, request => controllerFunction(
+                  CreateController<TController>(request)));
+
+        public static IRoutingTable MapPost<TController>(
+            this IRoutingTable routingTable,
+            string path,
+            Func<TController, Response> controllerFunction)
+            where TController : Controller
+            => routingTable.MapPost(path, request => controllerFunction(
+                  CreateController<TController>(request)));
+
+
+        private static TController CreateController<TController>(Request request)
+            =>(TController)Activator
+            .CreateInstance(typeof(TController),new[] {request});
+    }
+}
