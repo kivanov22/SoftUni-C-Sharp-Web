b304eecac12824db44abbb59f5af73664fa563c0
Kristian Ivanov <kriskopk@abv.bg>
2022-02-02 20:18:44 +0200

    Trips controller and Users controller created.

---
+++ C-Sharp-Web-Basics/Exam Preparation/SharedTrip/SharedTrip/Controllers/TripsController.cs
@@ -0,0 +1,6 @@
+﻿namespace SharedTrip.Controllers
+{
+    public class TripsController
+    {
+    }
+}
---
+++ C-Sharp-Web-Basics/Exam Preparation/SharedTrip/SharedTrip/Controllers/UsersController.cs
@@ -0,0 +1,34 @@
+﻿namespace SharedTrip.Controllers
+{
+    using MyWebServer.Controllers;
+    using MyWebServer.Http;
+    using SharedTrip.Data;
+    using SharedTrip.Models.Users;
+    using SharedTrip.Services;
+    using static Data.DataConstants;
+    public class UsersController: Controller
+    {
+        private readonly IValidator validator;
+        private readonly IPasswordHasher passwordHasher;
+        private readonly ApplicationDbContext data;
+
+        public UsersController(
+            IValidator validator,
+            IPasswordHasher passwordHasher,
+            ApplicationDbContext data)
+        {
+            this.validator = validator;
+            this.data = data;
+            this.passwordHasher = passwordHasher;
+        }
+
+        public HttpResponse Register() => View();
+
+        [HttpPost]
+        public HttpResponse Register(RegisterUserFormModel model)
+        {
+
+        }
+
+    }
+}
