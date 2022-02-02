72f32341eec1d018d643405c5d22d4e76e6fcd82
Kristian Ivanov <kriskopk@abv.bg>
2022-02-02 20:19:48 +0200

    More things done

---
+++ C-Sharp-Web-Basics/Exam Preparation/SharedTrip/SharedTrip/Models/Trps/AddTripFormModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SharedTrip.Models.Trps
+{
+    public class AddTripFormModel
+    {
+        public string StartingPoint { get; init; }
+
+        public string EndingPoint { get; init; }
+
+        public DateTime DepartureTime { get; init; }
+
+        public string Image { get; init; }
+
+        public int Seats { get; init; }
+
+        public string Description { get; init; }
+
+    }
+}
