fd4b9b31f9e1210863abf475ae39bf0250a66704
Kristian Ivanov <kriskopk@abv.bg>
2022-02-04 23:18:55 +0200

    Controllers with Login,Register and Add functionality

--- C-Sharp-Web-Basics/Exam Preparation/SharedTrip/SharedTrip/Controllers/TripsController.cs
+++ C-Sharp-Web-Basics/Exam Preparation/SharedTrip/SharedTrip/Controllers/TripsController.cs
@@ -1,6 +1,81 @@
-﻿namespace SharedTrip.Controllers
+﻿
+namespace SharedTrip.Controllers
 {
-    public class TripsController
+    using MyWebServer.Controllers;
+    using MyWebServer.Http;
+    using SharedTrip.Data;
+    using SharedTrip.Data.Models;
+    using SharedTrip.Models.Trps;
+    using SharedTrip.Services;
+    using System.Linq;
+    using static Data.DataConstants;
+
+    public class TripsController : Controller
     {
+        private readonly IValidator validator;
+        private readonly IPasswordHasher passwordHasher;
+        private readonly ApplicationDbContext data;
+
+        public TripsController(
+            IValidator validator,
+            IPasswordHasher passwordHasher,
+            ApplicationDbContext data)
+        {
+            this.validator = validator;
+            this.data = data;
+            this.passwordHasher = passwordHasher;
+        }
+
+
+        [Authorize]
+        public HttpResponse All()
+        {
+            var tripsQuery = this.data
+                .Trips
+                .AsQueryable();
+
+
+            var trips = tripsQuery
+                .Select(t => new TripListingViewModel
+                {
+                    Id= t.Id,
+                    StartingPoint = t.StartPoint,
+                    EndingPoint = t.EndPoint,
+                    DepartureTime = t.DepartureTime,
+                    Seats = t.Seats
+                })
+                .ToList();
+
+            return View(trips);
+        }
+
+
+        [Authorize]
+        [HttpPost]
+        public HttpResponse Add(AddTripFormModel model)
+        {
+            var modelErrors = this.validator.ValidateTrip(model);
+
+            if (modelErrors.Any())
+            {
+                return Error(modelErrors);
+            }
+
+            var trip = new Trip
+            {
+                StartPoint = model.StartingPoint,
+                EndPoint = model.EndingPoint,
+                DepartureTime = model.DepartureTime,
+                Seats = model.Seats,
+                Description = model.Description,
+                Image = model.Image
+            };
+
+            data.Trips.Add(trip);
+
+            data.SaveChanges();
+
+            return Redirect("/Trips/All");
+        }
     }
 }
--- C-Sharp-Web-Basics/Exam Preparation/SharedTrip/SharedTrip/Controllers/UsersController.cs
+++ C-Sharp-Web-Basics/Exam Preparation/SharedTrip/SharedTrip/Controllers/UsersController.cs
@@ -3,8 +3,10 @@
     using MyWebServer.Controllers;
     using MyWebServer.Http;
     using SharedTrip.Data;
+    using SharedTrip.Data.Models;
     using SharedTrip.Models.Users;
     using SharedTrip.Services;
+    using System.Linq;
     using static Data.DataConstants;
     public class UsersController: Controller
     {
@@ -27,7 +29,65 @@
         [HttpPost]
         public HttpResponse Register(RegisterUserFormModel model)
         {
+            var modelErrors = this.validator.ValidateUser(model);
 
+            if (this.data.Users.Any(u=>u.Username == model.Username))
+            {
+                modelErrors.Add($"User with '{model.Username}' username already exists.");
+            }
+
+            if (this.data.Users.Any(u => u.Email == model.Email))
+            {
+                modelErrors.Add($"User with '{model.Email}' e-mail already exists.");
+            }
+
+            if (modelErrors.Any())
+            {
+                return Error(modelErrors);
+            }
+
+            var user = new User
+            {
+                Username = model.Username,
+                Password = this.passwordHasher.HashPassword(model.Password),
+                Email = model.Email
+            };
+
+            data.Users.Add(user);
+            data.SaveChanges();
+
+            return Redirect("/Users/Login");
+        }
+
+        public HttpResponse Login() => View();
+
+        [HttpPost]
+        public HttpResponse Login(LoginUserFormModel model)
+        {
+            var hashedPassword = this.passwordHasher.HashPassword(model.Password);
+
+            var userId = this.data
+                .Users
+                .Where(u => u.Username == model.Username && u.Password == hashedPassword)
+                .Select(u => u.Id)
+                .FirstOrDefault();
+
+            if (userId == null)
+            {
+                return Error("Username and password combination is not valid.");
+            }
+
+            this.SignIn(userId);
+
+            return Redirect("/Trips/All");
+        }
+
+        [Authorize]
+        public HttpResponse Logout()
+        {
+            this.SignOut();
+
+            return Redirect("/");
         }
 
     }
