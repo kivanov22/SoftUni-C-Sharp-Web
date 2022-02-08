92240d66300e9cd292e723f57f73e9002837b683
Kristian Ivanov <kriskopk@abv.bg>
2022-02-08 17:03:51 +0200

    added custom Attributes

---
+++ C-Sharp-Web-Basics/Simple HTTP Server/BasicWebServer.Server/Attributes/AuthorizeAttribute.cs
@@ -0,0 +1,7 @@
+﻿namespace BasicWebServer.Server.Attributes
+{
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class)]
+    public class AuthorizeAttribute:Attribute
+    {
+    }
+}
---
+++ C-Sharp-Web-Basics/Simple HTTP Server/BasicWebServer.Server/Attributes/HttpMethodAttribute.cs
@@ -0,0 +1,13 @@
+﻿using BasicWebServer.Server.HTTP;
+
+namespace BasicWebServer.Server.Attributes
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public abstract class HttpMethodAttribute:Attribute
+    {
+        public Method HttpMethod { get; }
+
+        protected HttpMethodAttribute(Method httpMethod) 
+            =>HttpMethod = httpMethod;
+    }
+}
