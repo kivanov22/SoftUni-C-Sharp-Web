4230b8758b80a0dce3690b87e92c45d0536c12cc
Kristian Ivanov <kriskopk@abv.bg>
2022-02-08 18:18:44 +0200

    changed startup to map controllers

--- C-Sharp-Web-Basics/Simple HTTP Server/BasicWebServer.Demo/Startup.cs
+++ C-Sharp-Web-Basics/Simple HTTP Server/BasicWebServer.Demo/Startup.cs
@@ -1,24 +1,19 @@
 ﻿
 using BasicWebServer.Demo.Controllers;
+using BasicWebServer.Demo.Services;
 using BasicWebServer.Server;
 using BasicWebServer.Server.Routing;
 
 public class Startup
 {
     public static async Task Main()
-           => await new HttpServer(routes => routes
-         .MapGet<HomeController>("/", c => c.Index())
-         .MapGet<HomeController>("/Redirect", c => c.Redirect())
-         .MapGet<HomeController>("/HTML", c => c.Html())
-         .MapPost<HomeController>("/HTML", c => c.HtmlFormPost())
-         .MapGet<HomeController>("/Content", c => c.Content())
-         .MapPost<HomeController>("/Content", c=>c.DownloadContent())
-         .MapGet<HomeController>("/Cookies", c=>c.Cookies())
-         .MapGet<HomeController>("/Session", c=>c.Session())
-         .MapGet<UsersController>("/Login", c=> c.Login())
-         .MapPost<UsersController>("/Login", c=>c.LogInUser())
-        .MapGet<UsersController>("/Logout", c=>c.Logout())
-        .MapGet<UsersController>("/UserProfile", c=>c.GetUserData()))
-          .Start();
+    {
+          var server = new HttpServer(routes => routes
+          .MapControllers());
 
+        server.ServiceCollection
+            .Add<UserService>();
+
+        await server.Start();
+    }
 }
