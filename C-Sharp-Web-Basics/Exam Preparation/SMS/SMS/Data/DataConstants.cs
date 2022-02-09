b139882c3bd053fd3e04bae70baea931ff9a1eee
Kristian Ivanov <kriskopk@abv.bg>
2022-02-09 20:40:29 +0200

    added few more constants

--- C-Sharp-Web-Basics/Exam Preparation/SMS/SMS/Data/DataConstants.cs
+++ C-Sharp-Web-Basics/Exam Preparation/SMS/SMS/Data/DataConstants.cs
@@ -7,11 +7,13 @@
         public const int UsernameMinLength = 5;
         public const string ValidEmailRegex = @"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$";
         public const int PasswordMaxLength = 20;
+        public const int PasswordMinLength = 5;
 
         //Product
         public const int ProductNameMinLenght = 4;
         public const int ProductNameMaxLength = 20;
 
-
+        public const decimal ProductPriceMinLength = 0.05M;
+        public const int ProductPriceMaxLength = 1000;
     }
 }
