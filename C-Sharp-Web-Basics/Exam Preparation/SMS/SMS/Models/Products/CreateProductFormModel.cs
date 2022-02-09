50ea7e1a08532d68a792766f57efa1e5102ceaf8
Kristian Ivanov <kriskopk@abv.bg>
2022-02-09 20:39:37 +0200

    created models for user login/register and product creating

---
+++ C-Sharp-Web-Basics/Exam Preparation/SMS/SMS/Models/Products/CreateProductFormModel.cs
@@ -0,0 +1,9 @@
+﻿namespace SMS.Models.Products
+{
+    public class CreateProductFormModel
+    {
+        public string Name { get; init; }
+
+        public decimal Price { get; init; } 
+    }
+}
