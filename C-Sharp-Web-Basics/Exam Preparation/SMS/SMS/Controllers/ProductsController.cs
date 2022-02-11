04a60ff18064ccbc71f3e29e250f6e4b8b23d343
Kristian Ivanov <kriskopk@abv.bg>
2022-02-11 19:31:18 +0200

    UserController and started ProductController

--- C-Sharp-Web-Basics/Exam Preparation/SMS/SMS/Controllers/ProductsController.cs
+++ C-Sharp-Web-Basics/Exam Preparation/SMS/SMS/Controllers/ProductsController.cs
@@ -1,6 +1,68 @@
-﻿namespace SMS.Controllers
+﻿using MyWebServer.Controllers;
+using MyWebServer.Http;
+using SMS.Data;
+using SMS.Data.Models;
+using SMS.Models.Products;
+using SMS.Services;
+using System.Linq;
+
+namespace SMS.Controllers
 {
-    public class ProductsController
+    using static Data.DataConstants;
+    public class ProductsController:Controller
     {
+        private readonly IValidator validator;
+        private readonly IPasswordHasher passwordHasher;
+        private readonly SMSDbContext data;
+
+        public ProductsController(
+            IValidator validator,
+             IPasswordHasher passwordHasher,
+             SMSDbContext data)
+        {
+            this.validator = validator;
+            this.data = data;
+            this.passwordHasher = passwordHasher;
+        }
+
+        //Products ADD / CART CREATE / CARTS DETAILS
+
+        [Authorize]
+        public HttpResponse Create() => View();
+
+        [Authorize]
+        [HttpPost]
+        public HttpResponse Create(CreateProductFormModel model)
+        {
+            var modelErrors = this.validator.ValidateProducts(model);
+
+            if (modelErrors.Any())
+            {
+                return Error(modelErrors);
+            }
+
+            var product = new Product()
+            {
+                Name = model.Name,
+                Price = model.Price
+            };
+
+            data.Products.Add(product);
+            data.SaveChanges();
+
+            return Redirect("/");
+        }
+
+        [Authorize]
+        public HttpResponse Details(string cartId)
+        {
+
+        }
+
+        public HttpResponse AddProductToCart(string productId)
+        {
+
+        }
+
     }
 }
--- C-Sharp-Web-Basics/Exam Preparation/SMS/SMS/Controllers/UsersController.cs
+++ C-Sharp-Web-Basics/Exam Preparation/SMS/SMS/Controllers/UsersController.cs
@@ -1,9 +1,14 @@
 ﻿namespace SMS.Controllers
 {
+    using MyWebServer.Controllers;
+    using MyWebServer.Http;
     using SMS.Data;
+    using SMS.Data.Models;
+    using SMS.Models.Users;
     using SMS.Services;
+    using System.Linq;
     using static Data.DataConstants;
-    public class UsersController
+    public class UsersController : Controller
     {
         private readonly IValidator validator;
         private readonly IPasswordHasher passwordHasher;
@@ -19,6 +24,70 @@
             this.passwordHasher = passwordHasher;
         }
 
+        public HttpResponse Register() => View();
 
+
+        [HttpPost]
+        public HttpResponse Register(RegisterUserFormModel model)
+        {
+            var modelErrors = this.validator.ValidateUser(model);
+
+            if (this.data.Users.Any(u => u.Username == model.Username))
+            {
+                modelErrors.Add($"User with '{model.Username}' username already exists.");
+            }
+
+            if (this.data.Users.Any(u => u.Email == model.Email))
+            {
+                modelErrors.Add($"User with '{model.Email}' e-mail already exists.");
+            }
+
+            if (modelErrors.Any())
+            {
+                return Error(modelErrors);
+            }
+
+            var user = new User
+            {
+                Username = model.Username,
+                Email = model.Email,
+                Password = this.passwordHasher.HashPassword(model.Password)
+            };
+            data.Users.Add(user);
+            data.SaveChanges();
+
+            return Redirect("/Users/Login");
+        }
+
+        public HttpResponse Login() => View();
+
+        [HttpPost]
+        public HttpResponse Login(LoginUserFormModel model)
+        {
+            var hashedPassword = this.passwordHasher.HashPassword(model.Password);
+
+            var userId = this.data
+                .Users
+                .Where(u => u.Username == model.Username && u.Password == hashedPassword)
+                .Select(u => u.Id)
+                .FirstOrDefault();
+
+            if (userId == null)
+            {
+                return Error("Username and password combination is not valid.");
+            }
+
+            this.SignIn(userId);
+
+            return Redirect("/Products");
+        }
+
+        [Authorize]
+        public HttpResponse Logout()
+        {
+            this.SignOut();
+
+            return Redirect("/");
+        }
     }
 }
