85fc37a19bc14be2812f85168bf5ea61c451097c
Kristian Ivanov <kriskopk@abv.bg>
2022-02-11 19:33:11 +0200

    added Csharp code to cshtml views

--- C-Sharp-Web-Basics/Exam Preparation/SMS/SMS/StartUp.cs
+++ C-Sharp-Web-Basics/Exam Preparation/SMS/SMS/StartUp.cs
@@ -1,11 +1,12 @@
 ﻿namespace SMS
 {
     using System.Threading.Tasks;
-
+    using Microsoft.EntityFrameworkCore;
     using MyWebServer;
     using MyWebServer.Controllers;
     using MyWebServer.Results.Views;
     using SMS.Data;
+    using SMS.Services;
 
     public class StartUp
     {
@@ -16,7 +17,11 @@
                     .MapControllers())
                 .WithServices(services => services
                     .Add<SMSDbContext>()
+                    .Add<IValidator, Validator>()
+                    .Add<IPasswordHasher, PasswordHasher>()
                     .Add<IViewEngine, CompilationViewEngine>())
+            .WithConfiguration<SMSDbContext>(context => context
+            .Database.Migrate())
                 .Start();
     }
 }
