e6d252015e19462dca85a4b2b8a34759bdc43fc5
Kristian Ivanov <kriskopk@abv.bg>
2022-02-17 11:17:43 +0200

    CarShop added more services to startup

--- C-Sharp-Web-Basics/Exam Preparation/Car Shop/CarShop/Startup.cs
+++ C-Sharp-Web-Basics/Exam Preparation/Car Shop/CarShop/Startup.cs
@@ -1,21 +1,28 @@
-﻿
-namespace CarShop
+﻿namespace CarShop
 {
     using MyWebServer;
+    using CarShop.Data;
+    using System.Threading.Tasks;
     using MyWebServer.Controllers;
     using MyWebServer.Results.Views;
-    using System;
-    using System.Threading.Tasks;
+    using Microsoft.EntityFrameworkCore;
+    using CarShop.Services;
 
     public class Startup
     {
         public static async Task Main()
             => await HttpServer
-            .WithRoutes(routes => routes
-            .MapStaticFiles()
-            .MapControllers())
-            .WithServices(services => services
-            .Add<IViewEngine, CompilationViewEngine>())
-            .Start();
+                .WithRoutes(routes => routes
+                    .MapStaticFiles()
+                    .MapControllers())
+                .WithServices(services => services
+                .Add<ApplicationDbContext>()
+                .Add<IValidator,Validator>()
+                .Add<IUserService,UserService>()
+                .Add<IPasswordHasher,PasswordHasher>()
+                .Add<IViewEngine, CompilationViewEngine>())
+                .WithConfiguration<ApplicationDbContext>(context => context
+                    .Database.Migrate())
+                .Start();
     }
 }
